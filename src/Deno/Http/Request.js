557894445ef79e47d692c00d910e52d8f2ef6e90
Nathan Jaremko <nathan@jaremko.ca>
2022-06-22 12:29:48 -0700

    More data on request object

--- src/Deno/Http/Request.js
+++ src/Deno/Http/Request.js
@@ -3,6 +3,50 @@ export function body(req) {
     return req.body;
 }
 
+export function bodyUsed(req) {
+    return req.bodyUsed;
+}
+
+export function cache(req) {
+    return req.cache;
+}
+
+export function credentials(req) {
+    return req.credentials;
+}
+
+export function destination(req) {
+    return req.destination;
+}
+
+export function _headers(req) {
+    return req.headers.entries();
+}
+
+export function integrity(req) {
+    return req.integrity;
+}
+
+export function method(req) {
+    return req.method;
+}
+export function mode(req) {
+    return req.mode;
+}
+export function priority(req) {
+    return req.priority;
+}
+export function redirect(req) {
+    return req.redirect;
+}
+export function referrer(req) {
+    return req.referrer;
+}
+
+export function referrerPolicy(req) {
+    return req.referrerPolicy;
+}
+
 export function url(req) {
     return req.url;
 }
--- src/Deno/Http/Request.purs
+++ src/Deno/Http/Request.purs
@@ -1,10 +1,57 @@
-module Deno.Http.Request where
+module Deno.Http.Request
+  ( Request
+  , body
+  , bodyUsed
+  , cache
+  , credentials
+  , destination
+  , headers
+  , integrity
+  , method
+  , mode
+  , priority
+  , redirect
+  , referrer
+  , referrerPolicy
+  , url
+  ) where
 
+import Prelude
+import Data.Argonaut (Json, decodeJson)
+import Data.Either (fromRight)
+import Data.Map (Map)
 import Web.Streams.ReadableStream (ReadableStream)
 
 -- | A HTTP request object
 foreign import data Request :: Type
 
 foreign import body :: forall x. Request -> ReadableStream x
+
+foreign import bodyUsed :: Request -> Boolean
+
+foreign import cache :: Request -> String
+
+foreign import credentials :: Request -> String
+
+foreign import destination :: Request -> String
+
+foreign import _headers :: Request -> Json
+
+headers :: Request -> Map String String
+headers req = fromRight mempty $ decodeJson $ _headers req
+
+foreign import integrity :: Request -> String
+
+foreign import method :: Request -> String
+
+foreign import mode :: Request -> String
+
+foreign import priority :: Request -> String
+
+foreign import redirect :: Request -> String
+
+foreign import referrer :: Request -> String
+
+foreign import referrerPolicy :: Request -> String
 
 foreign import url :: Request -> String
